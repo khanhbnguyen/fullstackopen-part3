99880c38feeb3b37c95c26fb9be9f8f53d8879f3
khanhbnguyen <khanhbnguyen98@gmail.com>
2024-02-18 15:45:19 -0800

    updated after eslint

--- mongo.js
+++ mongo.js
@@ -11,41 +11,41 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema(
-    {
-        name: String,
-        number: String
-    }
+  {
+    name: String,
+    number: String
+  }
 )
 
 personSchema.set('toJSON', {
-    transform: (doc, ret) => {
-        ret.id = ret._id.toString()
-        delete ret._id
-        delete ret.__v
-    }
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString()
+    delete ret._id
+    delete ret.__v
+  }
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length < 4) {
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(JSON.stringify(person))
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(JSON.stringify(person))
     })
+    mongoose.connection.close()
+  })
 
 } else {
-    const person = new Person(
-        {
-            name: `${name}`,
-            number: `${number}`
-        }
-    )
+  const person = new Person(
+    {
+      name: `${name}`,
+      number: `${number}`
+    }
+  )
 
-    person.save().then(() => {
-        console.log(`Added ${name} number ${number} to phonebook!`)
-        mongoose.connection.close()
-    })
+  person.save().then(() => {
+    console.log(`Added ${name} number ${number} to phonebook!`)
+    mongoose.connection.close()
+  })
 }
 
